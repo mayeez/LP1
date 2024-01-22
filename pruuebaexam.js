9eb28ea12d8ab48332011c4b4541fec82ea16455
mayerli <abitorres045@gmail.com>
2024-01-22 13:21:27 -0300

    agg

--- pruuebaexam.js
+++ pruuebaexam.js
@@ -1,36 +1,36 @@
 //¿Qué tipo de dato tiene la siguente variable?
 //const c = "5";
-String
+console.log("String");
 //¿Qué tipo de dato tiene la siguente variable? const c = 91;
-Number
+console.log("Number");
 //¿Cuál de los dos es mejor?, línea 1 o línea 2?
 //let empty1 = undefined; //line 1
 //let empty2 = null; //line 2
-let empty2 = null;
+console.log ("linea 2");
 //¿Qué salida produce en consola?
 //let a = "Hello";
 //a = "world";
 //console.log(a);
-let rp= "Produce la palabra wordl";
+console.log("Produce la palabra wordl");
 //¿Qué se mostrará en la consola?
 //let a = "world";
 //let b = `Hello ${a}!`;
 //console.log(b);
-let rp2= "Se mostrara el hello wordl"
+console.log("Se mostrara el hello wordl");
 //¿Cuál es el valor de a?
 //let a = "Hello";
 //a = prompt("world");
 //console.log(a);
-let rp3="El valor de a es lo que se escribe en el cuadro de prompt";
+console.log("El valor de a es lo que se escribe en el cuadro de prompt");
 //¿Cuál es el valor de b en la consola?
 //let a = 5;
 //let b = 70;
 //let c = "5";
 //b++;
 //console.log(b);
-let rpb= "El valor de b en la consola es 71";
+console.log ("El valor de b en la consola es 71");
 //¿Cuál es el valor de este resultado? let result = 3 + 4 * 2 / 8;
-let rp4= "Sin el console.log sale null y con el resultado es 4";
+console.log("Sin el console.log sale null y con el resultado es 4");
 //¿Cuál es el valor de total y total2?
 //let firstNum = 5;
 //let secondNum = 10;
@@ -40,7 +40,7 @@ let rp4= "Sin el console.log sale null y con el resultado es 4";
 //console.log(total);
 //let total2 = 500 + 100 / 5 + total--;
 //console.log(total2);
-let rp5= "EL valor de total es: 16 y de total2 es: 536";
+console.log( "EL valor de total es: 16 y de total2 es: 536");
 //¿Qué se mostrará en la consola aquí?
 //const a = 5;
 //const b = 10;
@@ -49,4 +49,4 @@ let rp5= "EL valor de total es: 16 y de total2 es: 536";
 //console.log(true ||false);
 //console.log(a == 3 || b == 10);
 //console.log(a == 3 || b == 7);
-let rp6= "Se mostrara si las comparaciones son verdaderos o falsos, saldria asi: true-false-true-true-false";
+console.log("Se mostrara si las comparaciones son verdaderos o falsos, saldria asi: true-false-true-true-false");

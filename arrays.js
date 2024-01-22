eb70083733ca7800b4c369dac468d9c6611480e2
mayerli <abitorres045@gmail.com>
2024-01-22 19:04:10 -0300

    agregado

---
+++ arrays.js
@@ -0,0 +1,16 @@
+let razasDePerros= ["labrador", "pitbull", "chihuahua", "pastor aleman"];
+console.log({razasDePerros});
+
+//sobreescribiendo un array em la posicion -36
+razasDePerros[-36] = null;
+razasDePerros [1] = "dalmata";
+
+console.log({razasDePerros});
+
+console.log(razasDePerros[5]);
+console.log(razasDePerros[-36]);
+
+//uso de la propiedad lenght
+const cantidadRazaPerro= razasDePerros.length;
+console.log(razasDePerros.lenght);
+console.log(cantidadRazaPerro);

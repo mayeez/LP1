21e7219b7ef8ad2eb898a0653a77a555b9903673
mayerli <abitorres045@gmail.com>
2024-01-26 00:19:25 -0300

    agregado

---
+++ arraysmultidimensionales.js
@@ -0,0 +1,7 @@
+console.log("------------");
+let numerosarray = ["1", "2", "3"];
+
+let anidadoArrayNumeros = [numerosarray, numerosarray, numerosarray];
+console.log(anidadoArrayNumeros);
+
+console.log ( "El valor ", anidadoArrayNumeros [0] [1]);
---
+++ usar_objetos.js
@@ -0,0 +1,29 @@
+const yeti ={
+pelo: ""
+, nroCuernos: null
+, nroDeExtremiddades: null
+, tieneMascara: false
+, peso: null
+, altura: null
+,tieneOjos: false
+,danho: null
+, vida: null
+, tiempoDeCrecimiento: ""
+,tieneYetimitas: false
+, nroYetimitas: 0
+, velocidadMovimiento: 0
+, multiplicarYetimitas :  function () { }
+, ganarNiveles : () => { }
+, moverseaVelocidad :function(){}
+, recibirDanho : () => {}
+, atacarEdificios :  function () {}
+
+
+}
+
+yeti.tieneMascara= true;
+yeti.nroCuernos= 2;
+yeti.nroDeExtremiddades= 4;
+yeti.peso= 500;
+
+console.log(yeti);
